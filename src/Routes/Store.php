4c50e0cf8e6888607dda8372fdf2485448d004a6
PXgamer <owzie123@gmail.com>
2017-03-21 10:49:34 +0000

    Added Store routes

--- src/Routes/Store.php
+++ src/Routes/Store.php
@@ -2,10 +2,33 @@
 
 namespace pxgamer\Minimin\Routes;
 
+use pxgamer\Minimin\Smarter;
+
 class Store
 {
+    const STORE_PACKAGE_URL = 'https://packagist.org/search.json?tags=minimin-package';
+
+    public $S;
+
+    public function __construct()
+    {
+        $this->S = (new Smarter)->get();
+    }
+
     public function index()
     {
+        $this->S->display('store/index.tpl');
+    }
 
+    public function search()
+    {
+
+    }
+
+    private function getPackages($search_query = null)
+    {
+        $url = self::STORE_PACKAGE_URL . ($search_query ? '&q=' . urlencode($search_query) : '');
+        $cu = curl_init($url);
+        return json_decode(curl_exec($cu));
     }
 }
