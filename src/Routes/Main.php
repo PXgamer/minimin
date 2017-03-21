982aed73c918c45b0a48fc50db19095e339efed0
PXgamer <owzie123@gmail.com>
2017-03-21 11:05:16 +0000

    Added Smarter to Main class route

--- src/Routes/Main.php
+++ src/Routes/Main.php
@@ -2,10 +2,19 @@
 
 namespace pxgamer\Minimin\Routes;
 
+use pxgamer\Minimin\Smarter;
+
 class Main
 {
+    public $S;
+
+    public function __construct()
+    {
+        $this->S = Smarter::get();
+    }
+
     public function index()
     {
-
+        $this->S->display('index.tpl');
     }
 }
