0d4d6bcce40f1823517308b91c8ef90268eaaa16
PXgamer <owzie123@gmail.com>
2017-03-21 13:29:13 +0000

    Added Plugin installer functions

--- src/Plugins.php
+++ src/Plugins.php
@@ -17,4 +17,36 @@ class Plugins
 
         return $plugins;
     }
+
+    public static function install($vendor, $plugin_name)
+    {
+        if ($plugin_name == '' || !is_string($plugin_name)) {
+            return false;
+        }
+
+        $response = exec('cd "' . ROOT_PATH . '" && composer require ' . $vendor . '/' . $plugin_name);
+
+        if (!$response) {
+            return false;
+        }
+
+        $plugins = json_decode(file_get_contents(self::$json_path));
+        $pluginsInfo = Plugins::getPluginInfo($vendor, $plugin_name);
+        if (!empty($pluginsInfo)) {
+            $plugins[] = $pluginsInfo;
+            file_put_contents(self::$json_path, json_encode($plugins));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static function getPluginInfo($vendor = '', $plugin_name = '')
+    {
+        if (class_exists($vendor . '\\' . $plugin_name . '\\Plugin')) {
+            return ($vendor . '\\' . $plugin_name . '\\Plugin')::info();
+        } else {
+            return [];
+        }
+    }
 }
