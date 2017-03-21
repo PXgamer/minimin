e9e355c00b26089640481e8168febacc591bd94d
PXgamer <owzie123@gmail.com>
2017-03-21 09:58:02 +0000

    Initialised Minimin in the public index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
 
+use pxgamer\Minimin\Minimin;
+
 include '../vendor/autoload.php';
 
+define('DS', DIRECTORY_SEPARATOR, true);
+define('BASE_PATH', __DIR__ . DS, true);
+define('ROUTES', '\\pxgamer\\Minimin\\Routes\\');
+
+new Minimin();
