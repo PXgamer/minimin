203d19603bd8aae6dde3dd6c600081c5c042070f
PXgamer <owzie123@gmail.com>
2017-03-21 09:57:40 +0000

    Set up routing for the Minimin class

--- src/Minimin.php
+++ src/Minimin.php
@@ -2,7 +2,24 @@
 
 namespace pxgamer\Minimin;
 
+use System\App;
+use System\Request;
+use System\Route;
+
 class Minimin
 {
-	
+    public $controller;
+    public $request;
+    public $route;
+
+    public function __construct()
+    {
+        $this->controller = App::instance();
+        $this->request = Request::instance();
+        $this->route = Route::instance($this->request);
+
+        $this->route->any('/', [ROUTES . 'Main', 'index']);
+
+        $this->route->end();
+    }
 }
