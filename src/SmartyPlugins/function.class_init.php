d0b19e772a5db9380d5eaa5727a8b01b718ddc9a
PXgamer <owzie123@gmail.com>
2017-03-22 10:10:12 +0000

    Added a custom Smarty function

---
+++ src/SmartyPlugins/function.class_init.php
@@ -0,0 +1,10 @@
+<?php
+function smarty_function_class_init(array $params, &$smarty)
+{
+    $class = $params['class'];
+    if (isset($params['init'])) {
+        $smarty->assign($params['var'], new $class($params['init']));
+    } else {
+        $smarty->assign($params['var'], new $class());
+    }
+}
