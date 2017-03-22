51e04557261209e9d6c3916f74cafe6c06e283a2
PXgamer <owzie123@gmail.com>
2017-03-22 15:40:53 +0000

    Add 2 new Smarty modifiers

---
+++ src/SmartyPlugins/modifier.file_size.php
@@ -0,0 +1,16 @@
+<?php
+function smarty_modifier_file_size($int, $length = 4)
+{
+    $int = (int)$int;
+    switch (true) {
+        case $int < 1024:
+            return $int . 'B';
+        case $int < 1048576:
+            return trim_num($int / 1024, $length) . ' <span>KB</span>';
+        case $int < 1073741824:
+            return trim_num($int / 1048576, $length) . ' <span>MB</span>';
+        default:
+            return trim_num($int / 1073741824, $length) . ' <span>GB</span>';
+    }
+}
+	
