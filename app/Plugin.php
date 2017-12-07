ed02ad1385aa8ee4dcd274b94e96e9c25b2b63ab
pxgamer <owzie123@gmail.com>
2017-12-07 22:48:18 +0000

    Initialised Plugin files

---
+++ app/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Plugin.
+ *
+ * @property int    $id
+ * @property string $name
+ * @property string $link
+ * @property string $description
+ * @property string $app_namespace
+ */
+class Plugin extends Model
+{
+    //
+}
