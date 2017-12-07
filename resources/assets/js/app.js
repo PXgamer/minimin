013ef24a34bb27331f356179c25b49282ed13f0a
pxgamer <owzie123@gmail.com>
2017-12-07 22:00:17 +0000

    Installed Bulma

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,6 +5,7 @@
  */
 
 require('./bootstrap');
+require('./bulma-extensions');
 
 window.Vue = require('vue');
 
@@ -20,3 +20,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 const app = new Vue({
     el: '#app'
 });
+
+
+require('./bulma-extensions');
